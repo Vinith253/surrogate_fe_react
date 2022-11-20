772d02768b9fbaa5a219b0aa343c8347ed898cef
Shruthi <shruthi.ramachar@fibonalabs.com>
2022-11-20 12:24:48 +0530

    latest pull from main

--- src/components/commonComponent/Filters/state.tsx
+++ src/components/commonComponent/Filters/state.tsx
@@ -1,9 +1,45 @@
 import './style.scss';
+import * as React from 'react';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import TypoText from '../../../components/commonComponent/CustomText/Textfield';
-import { Select, Box, Typography, Grid } from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
+import { Box, Grid, Typography } from '@mui/material';
 
-function CurrentDayFilter() {
+function Filters() {
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  const names = [
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+  ];
+
+  const handleChange = (event: any) => {
+    const {
+      target: { value },
+    } = event;
+  };
+
   return (
     <div className="filters-styles">
       <div className="state-filter">
@@ -12,22 +48,45 @@ function CurrentDayFilter() {
             <Typography>State</Typography>
             <Select
               value={1}
+              labelId="demo-multiple-checkbox-label"
+              id="demo-multiple-checkbox"
               //   onChange={handleChange}
               displayEmpty
               inputProps={{ 'aria-label': 'Without label' }}
             >
-              {/* <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem> */}
+              {names.map((name: any) => (
+                <MenuItem key={name} value={name}>
+                  <Checkbox checked={true} />
+                  <ListItemText primary={name} />
+                </MenuItem>
+              ))}
             </Select>
           </Box>
         </Grid>
       </div>
     </div>
+    // <div>
+    //   <FormControl sx={{ m: 1, width: 300 }}>
+    //     <Select
+    //       labelId="demo-multiple-checkbox-label"
+    //       id="demo-multiple-checkbox"
+    //       multiple
+    //       // value={personName}
+    //       // onChange={handleChange}
+    //       input={<OutlinedInput label="Tag" />}
+    //       // renderValue={(selected: any) => selected.join(', ')}
+    //       // MenuProps={MenuProps}
+    //     >
+    //       {names.map((name: any) => (
+    //         <MenuItem key={name} value={name}>
+    //           <Checkbox checked={true} />
+    //           <ListItemText primary={name} />
+    //         </MenuItem>
+    //       ))}
+    //     </Select>
+    //   </FormControl>
+    // </div>
   );
 }
 
-export default CurrentDayFilter;
+export default Filters;
