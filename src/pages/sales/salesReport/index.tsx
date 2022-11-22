cc32f4a68ebcb68afbfa9d6ce2a9b7f69e50be0a
Shruthi <shruthi.ramachar@fibonalabs.com>
2022-11-22 15:22:27 +0530

    latest changes on sales report

--- src/pages/sales/salesReport/index.tsx
+++ src/pages/sales/salesReport/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 import TableComp from '../../../components/commonComponent/ListTable/ListTable';
 import {
@@ -7,6 +7,8 @@ import {
   statusRowHeading,
 } from './../dashboard/dashboard.const';
 import SalesReportNodata from '../../../assets/images/sales-report-no-data.svg';
+import DownloadIcon from '../../../assets/icons/download.svg';
+import MailIcon from '../../../assets/icons/mail.svg';
 import BtnContained from '../../../components/commonComponent/CustomText/Button/Contained';
 import BtnOutlined from '../../../components/commonComponent/CustomText/Button/Outlined';
 import SelectDropdown from '../../../components/commonComponent/CheckboxSelectDropdown';
@@ -15,9 +17,10 @@ import { salesReportFilterDropdown } from './salesReport.const';
 import { Typography, Stack, Box, Grid } from '@mui/material';
 
 function SalesReportList() {
+  const [isFiltered, setIsFiltered] = useState(false);
   return (
     <Stack className="sales-report-list">
-      <Stack className="filters-container">
+      <Stack className="container">
         <Stack className="underline">
           <Stack>
             <Typography variant="subtitle1" sx={{ letterSpacing: 0.5 }}>
@@ -33,31 +36,48 @@ function SalesReportList() {
           {salesReportFilterDropdown?.map((eachItem: any, index: number) => {
             return (
               <Grid item xs={3} key={index}>
-                <Typography>{eachItem?.label}</Typography>
+                <Typography className="dropdown-label">
+                  {eachItem?.label}
+                </Typography>
                 <SelectDropdown options={eachItem?.option} />
               </Grid>
             );
           })}
         </Grid>
         <Box className="button-container">
-          <BtnOutlined title="Reset" />
-          <BtnContained title="Search" />
+          <BtnOutlined title="Reset" onClick={() => setIsFiltered(false)} />
+          <BtnContained title="Search" onClick={() => setIsFiltered(true)} />
         </Box>
       </Stack>
-      <Stack className="no-data-container">
-        <img src={SalesReportNodata} className="no-data-img" />
-        <Typography variant="subtitle1" sx={{ letterSpacing: 0.5 }}>
-          Choose a preferred category to view more data/options
-        </Typography>
-      </Stack>
 
-      {/* <TableComp
-        viewPath="/sales/salesReportDetails"
-        rows={salesDashboardList}
-        statusRowsHeading={statusRowHeading}
-        listRowHeading={listRowHeading}
-        flag="dashboard"
-      /> */}
+      {isFiltered ? (
+        <Stack className="container">
+          <Stack className="underline">
+            <Typography variant="subtitle1" sx={{ letterSpacing: 0.5 }}>
+              Sales Data
+              <Typography className="icons-container">
+                <img src={DownloadIcon} className="icons" />
+                <img src={MailIcon} className="icons" />
+              </Typography>
+            </Typography>
+            <Typography variant="subtitle2" className="sub-label"></Typography>
+          </Stack>
+          <TableComp
+            viewPath="/sales/salesReportDetails"
+            rows={salesDashboardList}
+            statusRowsHeading={statusRowHeading}
+            listRowHeading={listRowHeading}
+            flag="sales-report"
+          />
+        </Stack>
+      ) : (
+        <Stack className="no-data-container">
+          <img src={SalesReportNodata} className="no-data-img" />
+          <Typography variant="subtitle1" sx={{ letterSpacing: 0.5 }}>
+            Choose a preferred category to view more data/options
+          </Typography>
+        </Stack>
+      )}
     </Stack>
   );
 }
