f5b031149b05e879d74dc6acaaad09348f767b0a
Narmatha <narmatha.s@fibonalabs.com>
2022-11-24 11:45:56 +0530

    org structure

--- src/pages/userManagement/orgStructure/landingScreen/index.tsx
+++ src/pages/userManagement/orgStructure/landingScreen/index.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { TabBar } from '../../../../components/commonComponent/customTab/CustomTab';
+import { dataList } from '../../../../interface/Types';
+// import HistoryLog from '../screens/historyLog/historyLog';
+import HistoryLog from '../../../productManagement/programmeManagement/screens/HistoryLog';
+
+import { OrganisationDetails } from '../screens/organisationDetails/organisationDetails';
 
 export const OrgStructure = () => {
-  return <div>OrgStructure</div>;
+  const TabListData: dataList = [
+    {
+      id: '1',
+      data: 'Organisation Details',
+      component: <OrganisationDetails />,
+    },
+
+    { id: '2', data: 'History Log', component: <HistoryLog /> },
+  ];
+  return (
+    <>
+      <TabBar data={TabListData} />
+    </>
+  );
 };
