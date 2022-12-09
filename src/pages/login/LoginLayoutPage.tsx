41ee28cdb4c32c5f89cc2f38b398e5c75c196a20
Vinith S <vinith.s@fibonalabs.com>
2022-12-09 10:35:26 +0530

    login screen changes

--- src/pages/login/LoginLayoutPage.tsx
+++ src/pages/login/LoginLayoutPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import './loginlayout.scss'
+import './loginlayout.scss';
 import { Box, Typography, Grid } from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
@@ -73,49 +73,41 @@ export const LoginLayoutPage = () => {
 
   return (
     <>
-      <Grid container className='login-layout-page' style={{ height: '100%' }}>
+      <Grid container className="login-layout-page" style={{ height: '100%' }}>
         <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-          <Box className='layout-container'
-           
-          >
-            <Box className='layout-head'
-              
-            >
-              <Box>
-                <Typography className='layout-heading'
-                  
-                >
-                  Surrogate Product for Credit Card Issuers
-                </Typography>
-                <Typography
-                 className='layout-heading-text'
-                  
-                >
-                  <Grid xs={12}>We Can handle</Grid>
-                  <Grid xs={12}>your Credit Card Issuance</Grid>
-                </Typography>
-                <Typography
-                className='layout-text'
-                  
-                >
-                  Disburse Credit Cards seamlessly! M2P handles your Credit Card
-                  issuance end-to-end with assured security and convenience.
-                </Typography>
-                <Box
-                className='layout-image'
-                  
-                >
-                  <img width={'75%'} height={'75%'} src={LoginImg} />
+          <Box className="layout-container">
+            <Box className="layout-head">
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                  height: '100%',
+                }}
+              >
+                <Box sx={{ width: '90%' }}>
+                  <Typography className="layout-heading">
+                    Surrogate Product for Credit Card Issuers
+                  </Typography>
+                  <Typography className="layout-heading-text">
+                    <Grid xs={12}>We Can handle</Grid>
+                    <Grid xs={12}>your Credit Card Issuance</Grid>
+                  </Typography>
+                  <Typography className="layout-text">
+                    Disburse Credit Cards seamlessly! M2P handles your Credit
+                    Card issuance end-to-end with assured security and
+                    convenience.
+                  </Typography>
+                </Box>
+                <Box className="layout-image">
+                  <img width={'90%'} height={'100%'} src={LoginImg} />
                 </Box>
               </Box>
             </Box>
           </Box>
         </Grid>
         <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-          <Box
-          className='layout-outlet'
-            
-          >
+          <Box className="layout-outlet">
             <Outlet />
           </Box>
         </Grid>
